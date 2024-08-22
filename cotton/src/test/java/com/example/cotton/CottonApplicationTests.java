9706c6737b64b34e7904bfe1b3025082e93880ce
DurgamAbhilash44 <durgamabhilash798@gmail.com>
2024-08-22 15:41:08 +0530

    first commit

---
+++ cotton/src/test/java/com/example/cotton/CottonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cotton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CottonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
